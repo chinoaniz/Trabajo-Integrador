94d3fbc4859777b307ccb9d769508c7309d147de
chinoaniz <97449765+chinoaniz@users.noreply.github.com>
2023-04-20 16:06:32 -0300

    arrglemos un poco el formulario falta validar bien

--- myJS.js
+++ myJS.js
@@ -1,1 +1,12 @@
-@MediaQueryList{ 90px}
+function enviarFormulario(event) {
+    event.preventDefault(); 
+    const nombre = document.getElementById("nombre").value;
+    const nombre = document.getElementById("apellido").value;
+    const nombre = document.getElementById("phone").value;
+    const nombre = document.getElementById("phone").value;
+    const email = document.getElementById("city").value; 
+    const nombre = document.getElementById("zip").value;
+    const nombre = document.getElementById("state").value;
+    const nombre = document.getElementById("checkTC").value;
+
+}
